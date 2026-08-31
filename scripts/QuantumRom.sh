8d6f41c49ef7fbc216ec691a9bfcf0a6b7245a5a
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-08-31 23:05:33 +0600

    Update QuantumRom.sh

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -538,7 +538,7 @@ DISABLE_FBE() {
         return 1
     fi
 
-    local fstab_files=$(grep -lr 'fileencryption' "${EXTRACTED_FIRM_DIR}/vendor/etc" 2>/dev/null)
+    local fstab_files=$(grep -lr 'fileencryption' "${EXTRACTED_FIRM_DIR}/vendor/etc")
 
     for i in $fstab_files; do
         if [ -f "$i" ]; then
@@ -562,7 +562,7 @@ DISABLE_FDE() {
         return 1
     fi
 
-    local fstab_files=$(grep -lr 'forceencrypt' "${EXTRACTED_FIRM_DIR}/vendor/etc" 2>/dev/null)
+    local fstab_files=$(grep -lr 'forceencrypt' "${EXTRACTED_FIRM_DIR}/vendor/etc")
 
     for i in $fstab_files; do
         if [ -f "$i" ]; then

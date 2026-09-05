d06daa5dd6fc63895178f5a0f1a3b64067b147c4
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-09-05 15:29:55 +0600

    Update QuantumRom.sh

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -104,6 +104,8 @@ DOWNLOAD_FIRMWARE() {
             echo "GoFile link detected"
             echo "Directory: ${BASH_REMATCH[1]}"
             python3 "$(pwd)/GoFileDownloader/downloader.py" "$CSC"
+			ls
+			pwd
             mv "$(pwd)/GoFileDownloader/Downloads/${BASH_REMATCH[1]}"/* "$(pwd)/$DOWN_DIR"/
 			return 0
         else
